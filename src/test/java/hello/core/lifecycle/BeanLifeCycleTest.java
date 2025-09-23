0b9f158db322a0042f8d14945590b94c3235da09
kyw0300 <kyw0300@naver.com>
2025-09-23 14:31:35 +0900

    빈 등록 초기화, 소멸 메서드

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -20,7 +20,7 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean
+        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://127.0.0.1:8080");
